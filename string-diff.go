f684fa0a3dac3c13d0a47e97320fe4658232defd
LIJU G CHACKO <liju.g.chacko@gmail.com>
2016-12-18 17:16:59 +0530

    Update string-diff.go

--- string-diff.go
+++ string-diff.go
@@ -3,76 +3,108 @@ import (
   "fmt"
 )
 
-
-
 func matchstring (old_word string,new_word string ) ([]int,[]int) {
-	i:=-1
-	j:=-1
-        markeroldword:=[]int{}
+	markeroldword:=[]int{}
 	markernewword:=[]int{}
- nextchar :
-	i=i+1
-	j=j+1
-	if i==len(old_word) && j==len(new_word) {
-		goto last
-	} else if i>len(old_word)-1 || j>len(new_word)-1 {
-		markeroldword=append(markeroldword,i)
-		markernewword=append(markernewword,j)
-		markeroldword=append(markeroldword,len(old_word)-1)
-		markernewword=append(markernewword,len(new_word)-1)
-		goto last
-	}
-	if old_word[i]==new_word[j] {
-		goto  nextchar 
-	} else {
-		markeroldword=append(markeroldword,i)
-		markernewword=append(markernewword,j)
-		valueofi:=-1
-		valueofj:=-1
-		down:=1
-		attempted:=true
-	nextsquare :
-		//searching in square
-		for k:=i;k<=i+down;k++ {
-		     for kj:=j;kj<=j+down;kj++ {
-			attempted=false
-			if k<len(old_word)-1 && kj<len(new_word)-1 {
-			    attempted=true    
-			    if old_word[k]==new_word[kj] {
-				    valueofi=k 
-				    valueofj=kj
-			    } 
+        var matching [200][200] int
+        old_word=old_word+" "
+	new_word=new_word+" "
+	for i:=0;i<len(old_word);i++ {
+		for j:=0;j<len(new_word);j++ {
+			if old_word[i] == new_word[j] {
+			      matching[i][j]=1
 			}
-		     }
 		}
-		if valueofi==-1 && valueofj==-1 && !attempted {
-			markeroldword=append(markeroldword,len(old_word)-1)
-			markernewword=append(markernewword,len(new_word)-1)
-			goto last
-		}
-		down=down+1
-		if valueofi==-1 && valueofj==-1 && attempted {
-			goto nextsquare
-		}
-		i=valueofi
-		j=valueofj
-		if markeroldword[len(markeroldword)-1]<=i-1 {
-			markeroldword=append(markeroldword,i-1)
-		} else {
-		        markeroldword=append(markeroldword,-1)
-		}
-		if markernewword[len(markernewword)-1]<=j-1 {
-			markernewword=append(markernewword,j-1)
-		} else {
-			markernewword=append(markernewword,-1)
-		}
-		goto nextchar
 	}
-last :  
-	//result format- [start1,end1,start2,end2 ...........]
-	 return markeroldword,markernewword
-	 
+
+	for j:=-len(new_word);j<=len(new_word);j++ {
+		for i:=len(old_word)-1;i>=0;i-- {
+			if j+i<len(new_word) && j+i>=0 {
+				if matching[i][i+j] > 0 && matching[i+1][i+j+1] > 0 {
+				      matching[i][i+j] = matching[i][i+j] + matching[i+1][i+j+1]
+				}
+			}
+		}
+	}
+
+	for j:=-len(new_word);j<=len(new_word);j++ {
+	        maxvalue:=0
+		for i:=0;i<len(old_word);i++ {
+			if j+i<len(new_word) && j+i>=0 {
+				if matching[i][i+j] > 0  {
+				      if maxvalue < matching[i][i+j] {
+					    maxvalue = matching[i][i+j]
+				      }
+				      matching[i][i+j] = maxvalue
+				} else if matching[i][i+j] ==0 {
+				      maxvalue = 0
+				}
+			}
+		}
+	}
+	
+	
+	
+	skip_i_till:=-1
+	opened:=false
+	if  matching[0][0] ==0 {
+		markeroldword=append(markeroldword,0)
+		markernewword=append(markernewword,0)
+		opened=true
+	}
+	lasty:=0
+	for i:=0;i<len(old_word);i++ {
+	        
+		if i >= skip_i_till {
+			maxj:=0
+			maxvalue:=0
+			for j:=0;j<len(new_word);j++ {
+				if matching[i][j] > maxvalue {
+				      maxj =  j
+				      maxvalue = matching[i][j]
+				}
+			}
+			if maxvalue>0  {
+			      if lasty>maxj {
+				    maxvalue=0
+			      }
+			      for k:=0;k<len(old_word);k++ {
+				    if maxvalue < matching[k][maxj] {
+					maxvalue=0
+				    }
+			      }
+			}
+			if maxvalue==0  {
+			} else {
+				lasty=maxj
+				skip_i_till = i + maxvalue
+				if i > 0 && opened {
+					markeroldword=append(markeroldword, i-1)
+					markernewword=append(markernewword, maxj-1)
+					
+					opened=false
+				}
+				//starting 
+				if (i + maxvalue) < len(old_word) && (maxj + maxvalue) < len(new_word) && maxvalue>0 && !opened{
+					markeroldword=append(markeroldword, i + maxvalue)
+					markernewword=append(markernewword, maxj + maxvalue)
+					
+					opened=true
+				}
+				
+			}
+			
+		}
+	}
+	if opened {
+		markeroldword=append(markeroldword,len(old_word)-2)
+	        markernewword=append(markernewword,len(new_word)-2)
+	}
+	//result format [start1,end1,start2,end2 ...........]
+	return markeroldword,markernewword
 }
+
+
 
 func printstringwithmarker (word string,marker []int ) {
         fmt.Printf("\n ")
